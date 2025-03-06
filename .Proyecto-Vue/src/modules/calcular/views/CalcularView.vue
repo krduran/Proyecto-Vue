8c76a615becda6e70b9aed99cc6607cadca9c61e
KENNYBALAZO\kd900 <kennydf9807@gmail.com>
2025-03-05 19:54:45 -0500

    Aplicamos propiedades computadas

---
+++ .Proyecto-Vue/src/modules/calcular/views/CalcularView.vue
@@ -0,0 +1,19 @@
+<template>
+<h3>Propiedades Comoutadas</h3>
+<input type="text" name="" id="" v-model="valor">
+{{ valorCalculado }}
+<ResultadosSection :resultado="valorCalculado"></ResultadosSection>
+</template>
+
+<script setup>
+import { ref , computed} from 'vue';
+import ResultadosSection from '../components/ResultadosSection.vue';
+
+const valor = ref('');
+const valorCalculado = computed(()=>{
+    return valor.value * 3
+});
+</script>
+
+<style scoped>
+</style>
